8734a59bec71587245b1e5248e3330074e289bf7
Mateusz Krzyściak <103695808+Asterfekt@users.noreply.github.com>
2023-02-05 00:08:06 +0100

    Początek Programu

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h> // do skorzystania z RAND
+
+#define x 5
+#define y 5
+
+
+
+int main()
+{
+    char MAPA[x][y];
+
+ for (int i = 0; i < x; i++)
+ {
+    
+ }
+ 
+
+    return 0;
+}
